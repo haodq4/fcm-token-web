6be07d7580b8005d651168a261521fc55e09e832
haodq5 <haodq5@fpt.com>
2025-08-12 14:06:30 +0700

    init

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,17 +1,3 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.3.0/firebase-app.js';
-import { getMessaging, onBackgroundMessage } from 'https://www.gstatic.com/firebasejs/10.3.0/firebase-messaging.js';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCbvoGeyqsnQPqXioPaivQl_CVw1KDtLqg",
-  authDomain: "happy-club-stg.firebaseapp.com",
-  projectId: "happy-club-stg",
-  storageBucket: "happy-club-stg.firebasestorage.app",
-  messagingSenderId: "428613555450",
-  appId: "1:428613555450:web:f3bbdbcff7af356e19e543",
-  measurementId: "G-SR5MNPQMP6"
-};
-
-const app = initializeApp(firebaseConfig);
 import { app } from './google-services-web-happyclub.js';
 import { getMessaging, onBackgroundMessage } from 'https://www.gstatic.com/firebasejs/10.3.0/firebase-messaging-sw.js';
 const messaging = getMessaging(app);
